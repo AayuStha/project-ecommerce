d2d847156a674f17d604851be2d223dc1cb8b354
AayuStha <aayustha21@gmail.com>
2024-03-29 21:01:12 +0545

    addded mail feature

--- Todo.md
+++ Todo.md
@@ -2,5 +2,5 @@
 2. cart design ui
 3. auto-generated email when buying
 4. buy now page with proper ui
-5. email notification - new user registration and buy products
-6. 
+5. email notification - buy products
+6. title of all pages
--- backend/signup.php
+++ backend/signup.php
@@ -1,3 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../css/style.css" class="css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <title>Sign Up - BagSalesNepal</title>
+    
+</head>
+<body>
+<div class="header">
+        <div class="navcont">
+            <div class="navbar">
+                <div class="logo">
+                    <a href="../index.html">
+                        BagShopNepal
+                    </a>
+                </div>
+                <nav>
+                    <ul id="items">
+                        <li><a href="../index.html" >Home</a></li>
+                        <li><a href="../products.php">Products</a></li>
+                        <li><a href="../about.html">About</a></li>
+                        <li><a href="../contact.html">Contact</a></li>
+                        <li><a href="../offers.html">Offers</a></li>
+                        <li><a href="../login.php" >Login</a></li>
+                        <li><a href="../signup.html" class="active">Signup</a></li>
+                    </ul>
+                </nav>
+                <button id="hamburger-menu" class="hamburger-menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <!-- <img src="images/cart.png" width="30px" height="30px"alt="cart"> -->
+            </div>
+        </div>
+    </div>
+
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -78,4 +124,58 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $conn->close();
 }
+?>
+<!-- Footer -->
 
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <h3>Download our App</h3>
+                    <p>Download  App for Android and ios mobile phone.</p>
+                    <div class="app-logo">
+                        <img src="backend/uploads/play-store.png" alt="">
+                        <img src="backend/uploads/app-store.png" alt="">
+                    </div>
+                </div>
+                <div class="footer-col-2">
+                    <h3>Newsletter</h3>
+                    <form action="..../subscribe.php" method="post">
+                        <input type="email" name="email" placeholder="Enter your email">
+                        <button type="submit">Subscribe</button>
+                    </form>
+                    <p>Our purpose is to sustainably make the pleasure and benefits of Bags Accessible to many.</p>
+                </div>
+                <table border="0" cellspacing="10" cellpadding="10">
+                    <tr>
+                        <td><h3>Follow us:</h3></td>
+                        <td><h3>Quick Links</h3></td>
+                    </tr>
+                    <tr>
+                        <td><a href="https://www.facebook.com">Facebook</a></td>
+                        <td><a href="policy.html">Privacy Policy</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="https://www.instagram.com">Instagram</a></td>
+                        <td><a href="shipping.html">Shipping policy</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="https://www.viber.com">Viber</a></td>
+                        <td><a href="return.html">Return Policy</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="https://www.whatsapp.com">Whatsapp</a></td>
+                        <td><a href="terms.html">Terms & Conditions</a></td>
+                    </tr>
+                </table>    
+            </div>
+            <hr>
+            <p class="copyright">© 2023 BagShop Nepal. All rights reserved. </p>
+        </div>
+    </div>
+
+    <script src="./js/button.js"></script>
+
+    
+</body>
+</html>
